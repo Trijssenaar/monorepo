d4a948eab64839101c0cb2efd4502a9faf3c07db
Sander Trijssenaar <sander@trijssenaar.net>
2023-01-02 14:11:12 +0100

    sa

---
+++ infra/terraform/deploy/demo/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.34.0"
+    }
+  }
+  backend "azurerm" {}
+
+  required_version = "1.3.4"
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+}
+
+locals {
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = local.resource_group_name
+  location = local.location
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}

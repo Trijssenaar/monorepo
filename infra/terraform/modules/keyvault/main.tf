f5b11dbcce031db1997c82f065a4f3ef8d40a57e
Sander Trijssenaar <sander@trijssenaar.net>
2023-01-06 10:23:44 +0100

    Terraform - Modules

--- infra/terraform/modules/keyvault/main.tf
+++ infra/terraform/modules/keyvault/main.tf
@@ -95,7 +95,7 @@ resource "azurerm_monitor_diagnostic_setting" "diag" {
 
 module "keyvaultcertificates" {
   source  = "trijssenaar.jfrog.io/infrastructure-terraform-local__monorepo/keyvaultcertificates/azurerm"
-  version = "0.1.0.40"
+  version = "0.1.0.41"
 
   for_each = var.certificates
 

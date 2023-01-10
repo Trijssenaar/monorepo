f793f61e372bfd2941de85391e4af941021d7921
Sander Trijssenaar <strijssenaar@xpirit.com>
2023-01-10 11:03:05 +0100

    Keyvault

--- infra/terraform/modules/shared-grid/main.tf
+++ infra/terraform/modules/shared-grid/main.tf
@@ -90,7 +90,7 @@ module "keyvault" {
 
 module "action_group_support" {
   source  = "trijssenaar.jfrog.io/infrastructure-terraform-local__monorepo/monitor-action-group/azurerm"
-  version = "0.1.1"
+  version = "0.1.4"
 
   resource_group_name = azurerm_resource_group.shared-grid.name
 
